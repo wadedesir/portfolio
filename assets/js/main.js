7cdffcbf4cf3ce82db90a96fed40db27b40c98cd
Wade <>
2021-03-17 18:19:39 -0400

    working on landing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-
+//https://www.tutorialspoint.com/online_css_editor.php Nav dropdown Menu
+let dropdownBtn = document.querySelector('.menu-btn');
+let menuContent = document.querySelector('.menu-content');
+dropdownBtn.addEventListener('click',()=>{
+   if(menuContent.style.display===""){
+      menuContent.style.display="block";
+   } else {
+      menuContent.style.display="";
+   }
+})
