e57d99debb2a699e6357b141c1fe9b432ad108de
Wade <>
2021-03-17 20:32:17 -0400

    updating landing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
+let photoGlow = 5
+let glowPhase = 'grow'
 
+window.onscroll = function () {
+    if (pageYOffset >= 200) {
+        document.querySelector('.leftBanner').style.display = "flex";
+    } else {
+       document.querySelector('.leftBanner').style.display = "none";
+    }
+};
+
+// setInterval( () => { glow() }, 130);
+//
+// function glow(){
+//   (glowPhase === 'grow') ? photoGlow += 5 : photoGlow -= 5
+//   document.querySelector('.headerLeft').style.filter = `drop-shadow(-1px 1px ${photoGlow}px orange)`
+//   glowPhase = (photoGlow > 20) ? 'shrink' : (photoGlow < 5) ? 'grow' : glowPhase
+// }
